4d0339aae671cce19dbdc667d8645df433b33d5b
romchik <r.bogdan26122001@gmail.com>
2023-02-26 17:37:48 +0200

    removing single quotes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": 'django.db.backends.postgresql_psycopg2',
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
