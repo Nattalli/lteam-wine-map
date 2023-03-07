e139a6193aed20805f7cc2e313a632487c9993e7
kseniia <kseniia@smartiot.cloud>
2023-03-08 00:11:25 +0200

    add login route

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,11 +10,13 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
+      <Route path='login' element={<Login />} />
     </Route>
   )
 );
