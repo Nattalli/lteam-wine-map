f655a84780822fc2d1f4a6975afcd1e1c4dfe7f8
kseniia <kseniia@smartiot.cloud>
2023-03-11 15:49:41 +0200

    accept token from Login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,17 @@ import { Layout } from 'antd';
 import Footer from './components/layout/Footer';
 import { Outlet } from 'react-router-dom';
 import './App.scss';
+import { useState } from 'react';
 
 function App() {
+  const [token, setToken] = useState('');
+
   return (
     <>
       <Layout className="layout">
         <Header />
         <Layout.Content className="main-section">
-          <Outlet />
+          <Outlet context={{ setToken }} />
         </Layout.Content>
         <Footer />
       </Layout>
