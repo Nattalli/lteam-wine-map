d6319ba54f09786e011babe54751f24e1edcab47
antos07 <trotsenko.ant07@gmail.com>
2023-03-12 15:53:39 +0100

    Fix line being too long

--- parsers/parsers/settings.py
+++ parsers/parsers/settings.py
@@ -79,7 +79,8 @@ AUTOTHROTTLE_TARGET_CONCURRENCY = 4.0
 AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# See:
+# https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 # HTTPCACHE_ENABLED = True
 # HTTPCACHE_EXPIRATION_SECS = 0
 # HTTPCACHE_DIR = "httpcache"
