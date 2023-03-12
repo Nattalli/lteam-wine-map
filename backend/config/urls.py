dfca2bfc38d4da451d16dfbbfee490d97160f9fa
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-03-12 15:15:31 -0400

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("django.contrib.auth.urls")),
-    path("auth/", include("authentication.urls"))
+    path("auth/", include("authentication.urls")),
     path("api/wine/", include("wine_map.urls")),
 ]
