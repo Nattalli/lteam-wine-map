a84817524075517d7926ffbb0f830a6fbf4a0c07
romchik <r.bogdan26122001@gmail.com>
2023-03-12 19:57:08 +0200

    checkstyle

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from django.dispatch import receiver
 from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
@@ -7,8 +5,7 @@ from django.core.mail import send_mail
 
 
 @receiver(reset_password_token_created)
-def password_reset_token_created(reset_password_token: Any):
-    print(type(reset_password_token))
+def password_reset_token_created(reset_password_token):
     email_plaintext_message = "{}?token={}".format(
         reverse("password_reset:reset-password-request"),
         reset_password_token.key)
