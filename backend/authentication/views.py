c4d189135c7e677c73d3b056c74d3febb2714d96
romchik <r.bogdan26122001@gmail.com>
2023-03-13 19:54:21 +0200

    resolving comments

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,6 +1,5 @@
 from rest_framework.permissions import AllowAny
 from .serializers import SignUpSerializer, UserSerializer
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from django.contrib.auth import get_user_model
 from rest_framework.request import Request
