17394d35651ea8f36c7e258e931e3d123e6f0fc1
kseniia <kseniia@smartiot.cloud>
2023-03-15 00:15:59 +0200

    add logout, render username in header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,10 +1,39 @@
-import { Layout, Button, Space, Typography, Row, Col } from 'antd';
-import { Link } from 'react-router-dom';
+import { Layout, Button, Space, Typography, Row, Col, Dropdown } from 'antd';
+import type { MenuProps } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import userIcon from '../../assets/img/user.svg';
 
 import './Header.scss';
+import { getRequest } from '../../api';
 
-export default function Header() {
-  // toDo pass user as prop and conditionally render button or username
+interface HeaderProps {
+  user: {
+    first_name: string;
+  },
+  setUser: Function;
+}
+
+
+export default function Header({ user, setUser}: HeaderProps) {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    await getRequest('/auth/logout/');
+    setUser({});
+
+    localStorage.removeItem('access');
+    localStorage.removeItem('refresh');
+    navigate('/login', { replace: true });
+  }
+
+  const menuItems: MenuProps['items'] = [
+    {
+      key: '1',
+      label: (
+        <div onClick={logout}>Logout</div>
+      ),
+    },
+  ];
   return (
     <Layout.Header className="header">
       <Row justify="space-between" align="middle">
@@ -18,9 +47,18 @@ export default function Header() {
             <Space size={[35, 5]} className="header-tabs">
               <Typography.Link>Каталог</Typography.Link>
               <Typography.Link>Тест</Typography.Link>
-              <Button type="primary" className="get-started-btn">
-                <Link to={'login'}>Увійти</Link>
-              </Button>
+              {!user.first_name ? (
+                <Button type="primary" className="get-started-btn">
+                  <Link to={'login'}>Увійти</Link>
+                </Button>
+              ) : (
+                <Dropdown menu={{ items: menuItems }} placement="bottom" arrow>
+                  <Typography.Link className="user-header">
+                    <span> {user.first_name}</span>
+                    <img src={userIcon} alt="user" />
+                  </Typography.Link>
+                </Dropdown>
+              )}
             </Space>
           </Row>
         </Col>
