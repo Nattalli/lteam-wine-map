5b37b8da0feb418643199727936a472612c68cc6
romchik <r.bogdan26122001@gmail.com>
2023-03-15 11:34:40 +0200

    fixing reset

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    "django_rest_passwordreset",
     "rest_framework",
     "rest_framework_simplejwt",
     "djoser",
