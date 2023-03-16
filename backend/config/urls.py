12efe13327858acabbaa4034bc38bdd778d6b6c2
kseniia <kseniia@smartiot.cloud>
2023-03-16 18:07:41 +0200

    formatting

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -31,8 +31,11 @@ urlpatterns = [
     path("auth/", include("authentication.urls")),
 
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/doc/swagger/", 
-        SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path(
+        "api/doc/swagger/", 
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
 ]
 
 if settings.DEBUG:
