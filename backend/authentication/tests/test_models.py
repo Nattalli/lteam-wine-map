1a4610398f49e32483c2815f1652c544b081a664
Nataliia Zakharchuk <62181026+Nattalli@users.noreply.github.com>
2023-03-19 16:49:07 +0200

    Update test_models.py

--- backend/authentication/tests/test_models.py
+++ backend/authentication/tests/test_models.py
@@ -13,13 +13,6 @@ class UserTest(TestCase):
         self.assertTrue(isinstance(self.user, get_user_model()))
         self.assertEqual(str(self.user), self.user.username)
 
-    def test_email_cannot_be_changed_to_empty(self) -> None:
-        email = self.user.email
-        self.user.email = None
-        self.user.refresh_from_db()
-
-        self.assertEqual(self.user.email, email)
-
     def test_empty_email_asserts_an_error(self) -> None:
         with self.assertRaises(ValueError):
             sample_user(email="", username="")
