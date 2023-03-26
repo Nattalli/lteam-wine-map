8972aedb6c03c88961e6d41f096a34028f1ce98e
Anton Trotsenko <37223374+antos07@users.noreply.github.com>
2023-03-26 18:46:11 +0200

    Update backend/wine_map/resources.py

--- backend/wine_map/resources.py
+++ backend/wine_map/resources.py
@@ -45,9 +45,7 @@ class WineResource(resources.ModelResource):
 
     def skip_row(self, instance: Wine, original: Wine, row: OrderedDict,
                  import_validation_errors: bool = None) -> bool:
-        if not instance.name.strip():
-            return True
-        if (instance.name, instance.brand_id) in self.processed:
+        if not instance.name.strip() and (instance.name, instance.brand_id) in self.processed:
             return True
         return super().skip_row(instance, original, row, import_validation_errors)
 
