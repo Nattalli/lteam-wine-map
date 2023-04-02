43f03e26072ff185a4353ef02a40aa551f4c5ef3
romchik <r.bogdan26122001@gmail.com>
2023-04-02 19:56:15 +0300

    adding wine of the day endpoint

--- backend/wine_map/wine_scheduler/updater.py
+++ backend/wine_map/wine_scheduler/updater.py
@@ -4,5 +4,5 @@ from wine_map.wine_scheduler.jobs import wine_of_the_day_generator
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(wine_of_the_day_generator, 'interval', seconds=86400)
+    scheduler.add_job(wine_of_the_day_generator, "interval", seconds=86400)
     scheduler.start()
